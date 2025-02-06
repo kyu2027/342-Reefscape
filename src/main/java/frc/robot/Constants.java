ba71b46558abc6e745b0d0592d4aa805d7d72c93
Wren! <rehuggins14@gmail.com>
2025-02-06 18:41:33 -0500

    Liam forgot to push

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,6 +13,36 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+  /*
+   * Constants are tbd
+   */
+  public static class IntakeConstants {
+    public static final int INTAKE_SENSOR = 1;
+    
+    public static final int INTAKE_MOTOR = 10;
+  
+    public static final int WRIST_ID = 9;
+    public static final int LEFT_ELEV_ID = 13;
+    public static final int RIGHT_ElEV_ID = 14;
+  
+    public static final int MAG_ENCODER = 7;
+  
+    public static final double INTAKE_SPEED = 0.6;
+    public static final double INTAKE_SHOOT_SPEED = 1;
+    public static final double FEED_SHOOTER_SPEED = 1; 
+    public static final double WRIST_SPEED = 0.7;
+  
+  public static final double LOW_WRIST_POS = 0.9;
+    public static final double HIGH_WRIST_POS = 0.30;
+    public static final double AMP_POS = 0.531; // In memoriam of 0.342 :(
+  
+    public static final double MAX_DISTANCE = 83;
+    
+    public static final double DESIRED_SPEED = 5100;
+    
+    public static final double DEFAULT_CURRENT = 30;
+  }
+  
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
---
+++ src/main/java/frc/robot/commands/WristToPosition.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import static frc.robot.Constants.IntakeConstants.HIGH_WRIST_POS;
+import static frc.robot.Constants.IntakeConstants.LOW_WRIST_POS;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Encoder;
+import com.revrobotics.jni.CANSparkJNI;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+
+import frc.robot.subsystems.Wrist;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class WristToPosition extends Command {
+  private Wrist wrist;
+  private XboxController joystick;
+  private PIDController pidController;
+  /** Creates a new WristToPosition. */
+  public WristToPosition() {
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
