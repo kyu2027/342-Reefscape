1b532649c6ea381c88df0ad286b998631b71f30a
kyu2027 <kyleyu1002@gmail.com>
2025-02-20 16:30:07 -0500

    Nuked wrist and what was remaining of climb

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,17 +10,10 @@ import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.commands.Elevator.MoveElevatorToPosition;
 import frc.robot.commands.Elevator.MoveElevatorWithJoystick;
-import frc.robot.commands.Wrist.WristToPosition;
-import frc.robot.commands.Wrist.WristWithJoystick;
 import frc.robot.subsystems.Elevator;
 import frc.robot.Constants.ElevatorConstants;
 
 import frc.robot.subsystems.*;
-
-import static frc.robot.Constants.WristConstants.ALGAE_POSITION;
-import static frc.robot.Constants.WristConstants.L1_POSITION;
-import static frc.robot.Constants.WristConstants.L2_POSITION;
-import static frc.robot.Constants.WristConstants.L4_POSITION;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
@@ -33,7 +26,6 @@ import edu.wpi.first.wpilibj2.command.button.POVButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.Subsystem;
-import frc.robot.Constants.WristConstants.*;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -45,7 +37,6 @@ public class RobotContainer {
   private final XboxController operator;
 
   private final Elevator elevator;
-  private final Wrist wrist;
 
   private final MoveElevatorToPosition moveElevatorProcessor;
   private final MoveElevatorToPosition moveElevatorL1;
@@ -60,21 +51,6 @@ public class RobotContainer {
   private final POVButton elevatorToL3;
   private final POVButton elevatorToL4;
 
-  //Because the angles are the same for both L2 & L3, there will only be an L2 command that will be used for both
-  private final WristToPosition wristToL1;
-  private final WristToPosition wristToL2;
-  private final WristToPosition wristToL4;
-  private final WristToPosition wristToAlgae;
-
-  private final WristWithJoystick wristWithJoy;
-
-  private final POVButton l1Button;
-  private final POVButton l2Button;
-  private final POVButton l4Button;
-  private final POVButton algaeButton;
-
-  private final JoystickButton climberToDeepClimb;
-
   private final CommandXboxController m_driverController;
   private final ExampleSubsystem m_exampleSubsystem;
 
@@ -86,16 +62,10 @@ public class RobotContainer {
 
     // Replace with CommandPS4Controller or CommandJoystick if needed
     m_driverController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
-    wrist = new Wrist();
+
     elevator = new Elevator();
 
     operator = new XboxController(1);
-
-    //Creates commands telling the wrist to go to different coral branches
-    wristToL1 = new WristToPosition(wrist, L1_POSITION);
-    wristToL2 = new WristToPosition(wrist, L2_POSITION);
-    wristToL4 = new WristToPosition(wrist, L4_POSITION);
-    wristToAlgae = new WristToPosition(wrist, ALGAE_POSITION);
 
     //Creates commands telling the elevator to go to different coral branches
     moveElevatorL1 = new MoveElevatorToPosition(elevator, ElevatorConstants.L1_HEIGHT);
@@ -104,15 +74,7 @@ public class RobotContainer {
     moveElevatorL4 = new MoveElevatorToPosition(elevator, ElevatorConstants.L4_HEIGHT);
     moveElevatorProcessor = new MoveElevatorToPosition(elevator, ElevatorConstants.PROCESSOR_HEIGHT);
 
-    wristWithJoy = new WristWithJoystick(operator, wrist);
     moveElevatorWithJoystick = new MoveElevatorWithJoystick(elevator, operator);
-
-
-    //Creating new buttons for L1, L2/L3, L4, and algae
-    l1Button = new POVButton(operator, 90);
-    l2Button = new POVButton(operator, 270);
-    l4Button = new POVButton(operator, 0);
-    algaeButton = new POVButton(operator, 180);
 
     //Operator buttons for elevator
     elevatorToL1 = new POVButton(operator, 180);
@@ -121,10 +83,6 @@ public class RobotContainer {
     elevatorToL4 = new POVButton(operator, 0);
     elevatorToProcessor = new JoystickButton(operator, XboxController.Button.kA.value);
 
-    //Operator button for climb
-    climberToDeepClimb = new JoystickButton(operator, XboxController.Button.kB.value);
-
-    wrist.setDefaultCommand(wristWithJoy);
     elevator.setDefaultCommand(moveElevatorWithJoystick);
 
     // Configure the trigger bindings
@@ -140,18 +98,12 @@ public class RobotContainer {
  * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
  * joysticks}.
  */
-private void configureBindings() {
+  private void configureBindings() {
   elevatorToL1.onTrue(moveElevatorL1); // down button on d-pad
   elevatorToL2.onTrue(moveElevatorL2); // left button on d-pad
   elevatorToL3.onTrue(moveElevatorL3); // right button on d-pad
   elevatorToL4.onTrue(moveElevatorL4); // top button on d-pad
   elevatorToProcessor.onTrue(moveElevatorProcessor); // the A button
-
-  //Moves the wrist to a certain position based on what button is pressed
-  l1Button.onTrue(wristToL1);
-  l2Button.onTrue(wristToL2);
-  l4Button.onTrue(wristToL4);
-  algaeButton.onTrue(wristToAlgae);
 
   // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
   new Trigger(m_exampleSubsystem::exampleCondition)
@@ -171,9 +123,9 @@ private void configureBindings() {
  *
  * @return the command to run in autonomous
  */
-public Command getAutonomousCommand() {
+  public Command getAutonomousCommand() {
   // An example command will be run in autonomous
   return Autos.exampleAuto(m_exampleSubsystem);
-}
+  }
 }
 
