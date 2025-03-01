8d1798a1345574234773f2af42e3b6007e7331b1
kyu2027 <kyleyu1002@gmail.com>
2025-03-01 14:01:48 -0500

    Added code to isFinished boolean

--- src/main/java/frc/robot/commands/Wrist/WristToPosition.java
+++ src/main/java/frc/robot/commands/Wrist/WristToPosition.java
@@ -38,6 +38,6 @@ public class WristToPosition extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return wrist.getPosition() == position;
   }
 }
