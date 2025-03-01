ab8d28f882ee4cd1272ad2486d29a31aee53d304
kyu2027 <kyleyu1002@gmail.com>
2025-03-01 17:54:06 -0500

    Added new constructor so elevator can hold position and give control back to MoveElevatorWithJoystick

--- src/main/java/frc/robot/commands/Elevator/MoveElevatorToPosition.java
+++ src/main/java/frc/robot/commands/Elevator/MoveElevatorToPosition.java
@@ -4,6 +4,8 @@
 
 package frc.robot.commands.Elevator;
 
+import static frc.robot.Constants.ElevatorConstants.ELEVATOR_ERROR;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Wrist;
@@ -15,17 +17,23 @@ public class MoveElevatorToPosition extends Command {
   private Wrist wrist;
 
   private double nextPosition;
+  private boolean hold;
 
   /** Creates a new MoveElevatorToPosition. */
-  public MoveElevatorToPosition(Elevator elevator, Wrist wrist, double nextPosition) {
+  public MoveElevatorToPosition(Elevator elevator, Wrist wrist, double nextPosition, boolean hold) {
 
     this.elevator = elevator;
     this.wrist = wrist;
     this.nextPosition = nextPosition;
+    this.hold = hold;
 
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(elevator);
 
+  }
+
+  public MoveElevatorToPosition(Elevator elevator, Wrist wrist, double nextPosition) {
+    this(elevator, wrist, nextPosition, false);
   }
 
   // Called when the command is initially scheduled.
@@ -41,11 +49,16 @@ public class MoveElevatorToPosition extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    elevator.holdPosition();
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return elevator.getEncoderPosition() == nextPosition;
+    if(!hold)
+      return (elevator.getEncoderPosition() > (nextPosition - ELEVATOR_ERROR)) && (elevator.getEncoderPosition() < (nextPosition + ELEVATOR_ERROR));
+    else
+      return false;
   }
 }
