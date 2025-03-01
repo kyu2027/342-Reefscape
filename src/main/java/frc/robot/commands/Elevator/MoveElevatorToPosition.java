19ce8467486032b764bc03af5e7d395c0eb5238c
kyu2027 <kyleyu1002@gmail.com>
2025-03-01 14:01:37 -0500

    implemented isSafe from wrist and boolean that ends the command

--- src/main/java/frc/robot/commands/Elevator/MoveElevatorToPosition.java
+++ src/main/java/frc/robot/commands/Elevator/MoveElevatorToPosition.java
@@ -6,18 +6,21 @@ package frc.robot.commands.Elevator;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Wrist;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class MoveElevatorToPosition extends Command {
 
   private Elevator elevator;
+  private Wrist wrist;
 
   private double nextPosition;
 
   /** Creates a new MoveElevatorToPosition. */
-  public MoveElevatorToPosition(Elevator elevator, double nextPosition) {
+  public MoveElevatorToPosition(Elevator elevator, Wrist wrist, double nextPosition) {
 
     this.elevator = elevator;
+    this.wrist = wrist;
     this.nextPosition = nextPosition;
 
     // Use addRequirements() here to declare subsystem dependencies.
@@ -32,7 +35,8 @@ public class MoveElevatorToPosition extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    elevator.ElevatorToPosition(nextPosition);
+    if(wrist.isSafe())
+      elevator.ElevatorToPosition(nextPosition);
   }
 
   // Called once the command ends or is interrupted.
@@ -42,6 +46,6 @@ public class MoveElevatorToPosition extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return elevator.getEncoderPosition() == nextPosition;
   }
 }
